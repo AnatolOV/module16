10cf16545ae0fbd0b2504bb68ffa4e1949d82af7
Anatoliy Oleynikov <oleinikovav.ao@gmail.com>
2024-11-27 22:59:45 +0300

    task3

--- module_16_3.py
+++ module_16_3.py
@@ -1,28 +1,41 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+
+from pydantic import BaseModel, conint, constr
 
 app = FastAPI()
+
 users = {'1': 'Имя: Example, возраст: 18'}
 
 
+class User(BaseModel):
+    username: constr(min_length=1, max_length=50)  # Имя пользователя от 1 до 50 символов
+    age: conint(ge=0, le=120)  # Возраст от 0 до 120 лет
+
+
 @app.get('/users')
-async def get_message():
+async def get_users():
     return users
 
 
-@app.post('/user/{username}/{age}')
-async def create_message(username: str, age: int):
+@app.post('/user/', response_model=str)
+async def create_user(user: User):
     user_id = str(int(max(users, key=int)) + 1)
-    user_info = f"Имя: {username}, возраст: {age}"
+    user_info = f"Имя: {user.username}, возраст: {user.age}"
     users[user_id] = user_info
     return f"User {user_id} is registered"
 
 
-@app.put('/user/{user_id}/{username}/{age}')
-async def update_message(user_id: int, username: str, age: int):
-    users[user_id] = f"Имя: {username}, возраст: {age}"
+@app.put('/user/{user_id}', response_model=str)
+async def update_user(user_id: str, user: User):
+    if user_id not in users:
+        raise HTTPException(status_code=404, detail='User not found')
+    users[user_id] = f"Имя: {user.username}, возраст: {user.age}"
     return f"The user {user_id} is updated"
 
 
-@app.delete('/user/{user_id}')
-async def delete_message(user_id):
+@app.delete('/user/{user_id}', response_model=str)
+async def delete_user(user_id: str):
+    if user_id not in users:
+        raise HTTPException(status_code=404, detail='User not found')
     users.pop(user_id)
+    return f"The user {user_id} is deleted"
